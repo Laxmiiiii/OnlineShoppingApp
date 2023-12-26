60dfc68706c965791a39b6dea4065a3c335cb36f
K Laxmi <k.laxmisetty111@gmail.com>
2023-12-26 22:33:43 +0530

    this is my first project

---
+++ FinalPrj/Angular-GroceryDeliverySystem/src/app/sign-up-admin/sign-up-admin.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AdminServiceService } from '../admin-service.service';
+import { User } from '../user';
+
+
+
+
+@Component({
+  selector: 'app-sign-up-admin',
+  templateUrl: './sign-up-admin.component.html',
+  styleUrls: ['./sign-up-admin.component.css']
+})
+export class SignUpAdminComponent implements OnInit{
+
+  user=new User( 0," " ,"","","","","","","","");
+  message=" "
+
+
+  ngOnInit(): void {
+    
+  }
+
+  constructor(private adminService:AdminServiceService,private route:Router){
+
+  }
+  public adminRegister(){
+  this.adminService.adminCreateService(this.user).subscribe(
+    data=>{console.log("Registration Success"), this.route.navigate(['/signInUser'])},
+    error=>{console.log("Registration Failed"),console.log(error),this.message="Registration Failed.Enter valid Credentials"}
+  )
+  }
+  public back(){
+  this.route.navigate(['register'])
+  }
+}
+
